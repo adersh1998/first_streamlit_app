82fc77192bb97e2fb5b336c2482821a1d329175c
Adersh Aniyan <35160272+adersh1998@users.noreply.github.com>
2023-02-25 00:17:54 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
